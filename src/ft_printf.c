0278de099f5efb63d63092e63280d383749e6415
Sauce Lisandra <slisandr@il-i3.21-school.ru>
2020-02-13 09:22:08 +0300

    Fails on 60 basic tests

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fcatina <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 23:25:50 by fcatina           #+#    #+#             */
-/*   Updated: 2020/02/13 09:16:38 by slisandr         ###   ########.fr       */
+/*   Updated: 2020/02/13 09:21:02 by slisandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ void	insert_format(const char *format, int i, va_list argptr, t_frm *tmp)
 		handle_c(tmp, argptr, &l);
 	else if (format[i] == 's')
 		handle_s(tmp, argptr);
+	else if (format[i] == '%')
+		handle_percent(tmp);
 }
 
 int		is_garbage(char c)
@@ -48,7 +50,7 @@ int		is_garbage(char c)
 	if ((c != 'd' && c != 'i' && c != 'o' &&	\
 		 c != 'u' && c != 'x' && c != 'X' && \
 		 c != 'c' && c != 's' && c != 'p') && \
-		c != '\0')
+		c != '%' && c != '\0')
 		return (1);
 	return (0);
 }
@@ -75,12 +77,6 @@ int		ft_printf(const char *format, ...)
 		else if (format[i + 1])
 		{
 			i += 1;
-			if (format[i] == '%')
-			{
-				handle_percent(&tmp);
-				ret += tmp.ret;
-				continue ;
-			}
 			tmp = process_flags(format, i); // new for ecrier flags and etc
 			while (is_garbage(format[i]))
 				i++;
