a2a6ebd9014ae1f5d26af3436a20c7d169cce707
ellaisa <ellaisa@bk.ru>
2020-02-18 06:16:23 +0300

    Fails on 9 basic tests

--- src/handle_s.c
+++ src/handle_s.c
@@ -12,22 +12,37 @@
 
 #include "libftprintf.h"
 
-/*
-** CHECKS:
-**
-** printf("%i %i\n", t, tmp->w);
-** printf("%i %i %i\n", tmp->w, t, tmp->minus);
-*/
-
 void	handle_s(t_frm *tmp, va_list argptr)
 {
 	char	*s;
+	char	*s_tmp;
 	int		t;
 
+	s_tmp = NULL;
 	t = 0;
 	s = va_arg(argptr, char*);
 	if (s == NULL)
 		s = ft_strdup("(null)");
+	if (s != NULL && tmp->w >= tmp->precision && s[0] == '\0' && tmp->precision > 0 && tmp->w > 0)
+		handle_minus(tmp, " ", 1, "1");
+	if (tmp->precision > 0)
+	{
+		if (tmp->w <= tmp->precision)
+			s[tmp->precision] = '\0';
+		else if (tmp->minus)
+		{
+			ft_memset((void *)s + tmp->precision, ' ', tmp->w - tmp->precision);
+			s[tmp->w] = '\0';
+		}
+		else
+		{
+			s_tmp = ft_strdup(s);
+			ft_memset((void *)s, ' ', tmp->w);
+			s[tmp->w] = '\0';
+			ft_strncpy(s + (tmp->w - tmp->precision) - 1, s_tmp, tmp->precision);
+			s[tmp->w - 1] = '\0';
+		}
+	}
 	if (tmp->w > 0)
 	{
 		t = ((tmp->minus) ? (ft_strlen(s)) : (ft_strlen(s) + 1));
@@ -37,12 +52,12 @@ void	handle_s(t_frm *tmp, va_list argptr)
 			tmp->w = tmp->w - t;
 		while (tmp->w >= 0 && tmp->minus == 0)
 		{
-			ft_putchar(' ');
-			tmp->ret += 1;
+			putchar_and_count(' ', tmp);
 			(tmp->w)--;
 		}
 	}
 	ft_putstr(s);
 	tmp->ret += ft_strlen(s);
-	handle_minus(tmp, " ", 1, "1");
+	if (tmp->precision <= 0)
+		handle_minus(tmp, " ", 1, "1");
 }
